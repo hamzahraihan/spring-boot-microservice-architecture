1010bb29f296dd016fd43cffe0eefb7306e5070c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2026-06-12 17:13:41 +0700

    Use MariaDB container in tests
    Switch Testcontainers MySQLContainer to use the 'mariadb:11.2' image and mark it as a compatible substitute for MySQL via asCompatibleSubstituteFor("mysql"). This allows tests expecting a MySQL container to run against MariaDB 11.2 while preserving compatibility.

--- inventory-service/src/test/java/com/kimu/microservice/inventory/TestcontainersConfiguration.java
+++ inventory-service/src/test/java/com/kimu/microservice/inventory/TestcontainersConfiguration.java
@@ -12,7 +12,7 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	MySQLContainer mysqlContainer() {
-		return new MySQLContainer(DockerImageName.parse("mysql:latest"));
+		return new MySQLContainer(DockerImageName.parse("mariadb:11.2").asCompatibleSubstituteFor("mysql"));
 	}
 
 }
