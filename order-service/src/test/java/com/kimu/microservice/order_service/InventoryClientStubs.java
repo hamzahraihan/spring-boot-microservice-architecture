d49a4faf6aa900eaecead32f26448d8a7e1c89fe
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2026-06-12 17:14:52 +0700

    Add WireMock stub for Inventory client
    Introduce InventoryClientStubs test utility that provides stubInventoryCall(skuCode, quantity). It stubs a GET to /api/inventory?skuCode={skuCode}&quantity={quantity} and returns HTTP 200 with Content-Type: application/json and body "true" to simulate the inventory service in tests.

---
+++ order-service/src/test/java/com/kimu/microservice/order_service/InventoryClientStubs.java
@@ -0,0 +1,12 @@
+package com.kimu.microservice.order_service;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+
+public class InventoryClientStubs {
+
+    public static void stubInventoryCall(String skuCode, Integer quantity) {
+        stubFor(get(urlEqualTo("/api/inventory?skuCode=" + skuCode + "&quantity=" + quantity)).willReturn(
+                aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody("true")));
+    }
+}
