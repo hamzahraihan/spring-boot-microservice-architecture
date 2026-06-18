0977b1e920d80efed92a370c82e227351014ab0e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2026-06-18 10:32:12 +0700

    Add OpenAPI (springdoc) support to inventory service
    Introduce springdoc OpenAPI support: add springdoc-openapi-starter-webmvc-ui and springdoc-openapi-starter-webmvc-api dependencies (v3.0.2) to pom.xml, add OpenAPIConfig providing API metadata (title, description, version, license, and external docs), and configure Swagger UI and API docs endpoints in application.properties (springdoc.swagger-ui.path and springdoc.api-docs.path).

---
+++ inventory-service/src/main/java/com/kimu/microservice/inventory/config/OpenAPIConfig.java
@@ -0,0 +1,27 @@
+package com.kimu.microservice.inventory.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI inventoryServiceAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Inventory service API")
+                        .description("This is the REST API for Inventory Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("You can refer to the Inventory service wiki documentation")
+                        .url("https://inventory-service-dummy-url.com/docs"));
+    }
+
+}
