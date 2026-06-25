17191db5b146623edd1d8a129d842b5ede509f1f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2026-06-25 19:10:38 +0700

    Update create-order.tsx

--- frontend/src/feature/order/api/create-order.tsx
+++ frontend/src/feature/order/api/create-order.tsx
@@ -1,5 +1,5 @@
 import { api } from "../../../lib/api-client";
-import type { OrderType } from "../type/ordertype";
+import type { OrderType } from "../../../types/api";
 
 export const createOrder = (data: OrderType, token?: string) => {
   return api.post("/api/order", token, data);
