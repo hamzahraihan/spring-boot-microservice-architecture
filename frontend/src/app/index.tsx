72054b5426374534c549335b62cefbff59d07689
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2026-06-25 19:10:12 +0700

    Home: add product list and navigation links
    Replace the previous ProductPage with ProductList on the home screen and add navigation links. Imports updated to use Link from react-router and ProductList from the feature folder. Header now includes a linked "Product" title and an "Add Product" link, and the product list is rendered below.

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,11 +1,23 @@
-import ProductPage from "./product";
 import "./home.css";
+import { Link } from "react-router";
+import ProductList from "../feature/product/components/product-list";
 
 function Home() {
   return (
     <div className="container">
-      <h1>Product</h1>
-      <ProductPage />
+      <div className="flex" style={{ paddingBottom: 20 }}>
+        <h1>
+          <Link
+            to="/product"
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            Product
+          </Link>
+        </h1>
+        <Link to="/add-product">Add Product</Link>
+      </div>
+
+      <ProductList />
     </div>
   );
 }
